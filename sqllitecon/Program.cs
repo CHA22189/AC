38c5f23bba46449f759264f27b8948c5eb6ff09f
AC <115209693+MrLegond@users.noreply.github.com>
2023-02-17 09:28:46 +0100

    Add files via upload
    17.2.2023 Hausübung SQLite

---
+++ sqllitecon/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string connectionString = "Data Source=C:\\Users\\Aminc\\OneDrive - HTL Spengergasse\\3. Klasse\\SWP\\Graf\\MySQL\\sqllite_test";
+
+            
+            string query = "SELECT Wert, Spalte2 FROM mytable";
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            Console.WriteLine(reader["Wert"].ToString()+reader["Spalte2"]);
+                           
+                            
+                        }
+                    }
+                }
+
+                connection.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
